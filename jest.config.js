ee9f880b34cd59eb014886c5b37bbf7563e41ba7
ktscates <cates2506@gmail.com>
2024-10-07 15:14:51 +0200

    initial commit & project setup

---
+++ jest.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  preset: "jest-preset-angular",
+  setupFilesAfterEnv: ["<rootDir>/setupJest.ts"],
+  testEnvironment: "jsdom",
+  testMatch: ["**/+(*.)+(spec).+(ts)"],
+  transform: {
+    "^.+.(ts|mjs|js|html)$": [
+      "jest-preset-angular",
+      {
+        tsconfig: "<rootDir>/tsconfig.spec.json",
+        stringifyContentPathRegex: "\\.(html|svg)$",
+      },
+    ],
+  },
+  transformIgnorePatterns: ["node_modules/(?!(.*\\.mjs$|d3-.*))"],
+  snapshotSerializers: [
+    "jest-preset-angular/build/serializers/no-ng-attributes",
+    "jest-preset-angular/build/serializers/ng-snapshot",
+    "jest-preset-angular/build/serializers/html-comment",
+  ],
+  moduleFileExtensions: ["ts", "html", "js", "json"],
+  collectCoverage: true,
+  coverageReporters: ["html", "text", "lcov", "json"],
+  coverageDirectory: "coverage/jest-demo",
+  coverageThreshold: {
+    global: {
+      statements: 80,
+      branches: 70,
+      functions: 80,
+      lines: 80,
+    },
+  },
+};
